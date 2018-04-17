2a0d975f48165a839567edc14bf362c6835dca92
brunofilipe <bruno.filipe1996@hotmail.com>
2018-04-17 14:55:23 +0100

    starting back-end spring project

---
+++ back-end/eduWikiAPI/src/main/kotlin/isel/leic/ps/eduWikiAPI/EduWikiApiApplication.kt
@@ -0,0 +1,11 @@
+package isel.leic.ps.eduWikiAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EduWikiApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<EduWikiApiApplication>(*args)
+}
