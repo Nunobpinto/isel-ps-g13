f0867c450ff1d1bdedc5d5bd9eae897218e40b29
Nunobpinto <nunobernardopinto@gmail.com>
2018-05-07 18:46:50 +0100

    continued database creation script

--- back-end/database/eduwiki.sql
+++ back-end/database/eduwiki.sql
@@ -44,6 +44,7 @@ CREATE TABLE programme (
 -- Add column column restricting the terms where it can be lectured (winter only, summer only, both)
 CREATE TABLE course (
   course_id SERIAL,
+  organization_id INTEGER REFERENCES organization,
   course_version INTEGER UNIQUE NOT NULL,
   course_full_name varchar(100) UNIQUE NOT NULL,
   course_short_name varchar(10) UNIQUE NOT NULL,
@@ -113,13 +114,11 @@ CREATE TABLE exam (
 );
 
 CREATE TABLE course_class (
-  course_class_id SERIAL,
-  course_class_version INTEGER,
   course_id INTEGER REFERENCES course,
   class_id INTEGER,
   term_id INTEGER,
   FOREIGN KEY (class_id, term_id) REFERENCES class(class_id, term),
-  PRIMARY KEY (course_class_id, course_id, class_id, term_id)
+  PRIMARY KEY (course_id, class_id, term_id)
 );
 
 CREATE TABLE class_misc_unit (
@@ -128,8 +127,7 @@ CREATE TABLE class_misc_unit (
   course_id INTEGER,
   class_id INTEGER,
   term_id INTEGER,
-  course_class_id INTEGER,
-  FOREIGN KEY (course_id, class_id, term_id, course_class_id) REFERENCES course_class(course_id, class_id, term_id, course_class_id),
+  FOREIGN KEY (course_id, class_id, term_id) REFERENCES course_class(course_id, class_id, term_id),
   PRIMARY KEY (id)
 );
 
@@ -161,7 +159,7 @@ CREATE TABLE student (
   PRIMARY KEY (student_username)
 );
 
-create TABLE reputation (
+CREATE TABLE reputation (
   reputation_id SERIAL,
   reputation_points INTEGER NOT NULL,
   reputation_rank student_rank NOT NULL,
@@ -180,12 +178,126 @@ CREATE TABLE reputation_log (
   PRIMARY KEY (reputation_log_id)
 );
 
+CREATE TABLE student_course_class (
+	username VARCHAR(20) NOT NULL REFERENCES student,
+	course_id INTEGER NOT NULL REFERENCES course,
+	class_id INTEGER,
+	PRIMARY KEY (username, course_id)
+);
+
 --------------------------
 -- Create Staging Tables
 --------------------------
 
+CREATE TABLE programme_stage (
+	programme_id SERIAL,
+	programme_full_name varchar(100) UNIQUE NOT NULL,
+	programme_short_name varchar(10) UNIQUE NOT NULL,
+	programme_academic_degree varchar(50) NOT NULL,
+	programme_total_credits INTEGER,
+	programme_duration INTEGER,
+	created_by VARCHAR(20),
+	vote_count INTEGER DEFAULT 0,
+	PRIMARY KEY (programme_id)
+);
 
+CREATE TABLE course_stage (
+	course_id SERIAL,
+	organization_id INTEGER REFERENCES organization,
+	course_full_name varchar(100) UNIQUE NOT NULL,
+	course_short_name varchar(10) UNIQUE NOT NULL,
+	created_by VARCHAR(20),
+	vote_count INTEGER DEFAULT 0,
+	PRIMARY KEY (course_id)
+);
 
---------------------------
--- Create Version Tables
---------------------------
+CREATE TABLE course_programme_stage (
+  course_id INTEGER REFERENCES course,
+  programme_id INTERVAL REFERENCES programme,
+  course_lectured_term varchar(50) NOT NULL,
+  course_optional BOOLEAN NOT NULL,
+  course_credits INTEGER NOT NULL,
+	created_by VARCHAR(20),
+	vote_count INTEGER DEFAULT 0,
+  PRIMARY KEY (course_id, programme_id)
+);
+
+CREATE TABLE class_stage (
+	class_id SERIAL,
+	class_name VARCHAR(10),
+	term INTEGER REFERENCES term,
+	created_by VARCHAR(20),
+	vote_count INTEGER DEFAULT 0,
+	PRIMARY KEY (class_id, term)
+);
+
+CREATE TABLE course_class_stage (
+	course_id INTEGER REFERENCES course,
+	class_id INTEGER,
+	term_id INTEGER,
+	created_by VARCHAR(20),
+	vote_count INTEGER DEFAULT 0,
+	FOREIGN KEY (class_id, term_id) REFERENCES class(class_id, term),
+	PRIMARY KEY (course_id, class_id, term_id)
+);
+
+-- CREATE TABLE course_term_stage (
+--   course_id INTEGER REFERENCES course,
+--   term_id INTEGER REFERENCES term,
+--   PRIMARY KEY (course_id, term_id)
+-- );
+
+CREATE TABLE course_misc_unit_stage (
+  id SERIAL,
+  misc_type course_misc_unit_type,
+  course_id INTEGER,
+  term_id INTEGER,
+  FOREIGN KEY (course_id, term_id) REFERENCES course_term(course_id, term_id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE work_assignment_stage (
+  	id INTEGER REFERENCES course_misc_unit,
+  	sheet VARCHAR(100),
+  	supplement VARCHAR(100),
+  	due_date date,
+  	individual BOOLEAN,
+  	late_delivery BOOLEAN,
+  	multiple_deliveries BOOLEAN,
+  	requires_report BOOLEAN,
+	created_by VARCHAR(20),
+	vote_count INTEGER DEFAULT 0,
+ 	PRIMARY KEY (id)
+);
+
+CREATE TABLE exam_stage (
+  	id INTEGER REFERENCES course_misc_unit,
+  	sheet VARCHAR(100),
+  	due_date date,
+  	type exam_type,
+  	phase VARCHAR(30),
+  	location varchar(30),
+	created_by VARCHAR(20),
+	vote_count INTEGER DEFAULT 0
+  	PRIMARY KEY (id)
+);
+
+CREATE TABLE lecture (
+  id INTEGER REFERENCES class_misc_unit,
+  weekday weekday,
+  begins TIME,
+  duration INTERVAL,
+  location varchar(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE homework (
+  id INTEGER REFERENCES class_misc_unit,
+  sheet VARCHAR(100),
+  due_date DATE,
+  late_delivery BOOLEAN,
+  multiple_deliveries BOOLEAN,
+  requires_report BOOLEAN,
+  PRIMARY KEY (id)
+);
+-- Falta alguns stages, reports e versions
