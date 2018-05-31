ff1530c6b8654906b1a1c82811c4243a6ada0b43
Nunobpinto <nunobernardopinto@gmail.com>
2018-05-31 22:59:12 +0100

    slightly changed voteInputModel

--- back-end/eduWikiAPI/src/main/kotlin/isel/leic/ps/eduWikiAPI/domain/inputModel/VoteInputModel.kt
+++ back-end/eduWikiAPI/src/main/kotlin/isel/leic/ps/eduWikiAPI/domain/inputModel/VoteInputModel.kt
@@ -1,6 +1,9 @@
 package isel.leic.ps.eduWikiAPI.domain.inputModel
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 class VoteInputModel (
         val vote: String,
-        val username: String
+        @JsonProperty("created_by")
+        val createdBy: String
 )
