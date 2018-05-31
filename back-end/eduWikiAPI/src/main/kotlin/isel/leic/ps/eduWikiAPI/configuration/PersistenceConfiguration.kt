b41f6ef5ea5123a44a0bd91d456441962c6cc272
Nunobpinto <nunobernardopinto@gmail.com>
2018-05-31 22:55:37 +0100

    jdbi Handle is now a request scope bean in order to confirm that only one connection per request is created

--- back-end/eduWikiAPI/src/main/kotlin/isel/leic/ps/eduWikiAPI/configuration/PersistenceConfiguration.kt
+++ back-end/eduWikiAPI/src/main/kotlin/isel/leic/ps/eduWikiAPI/configuration/PersistenceConfiguration.kt
@@ -1,19 +1,29 @@
 package isel.leic.ps.eduWikiAPI.configuration
 
+import org.jdbi.v3.core.Handle
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.kotlin.KotlinPlugin
 import org.jdbi.v3.sqlobject.kotlin.KotlinSqlObjectPlugin
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.context.annotation.RequestScope
 import javax.sql.DataSource
 
 @Configuration
 class PersistenceConfiguration {
 
     @Bean
-    fun jdbiBean(dataSource: DataSource): Jdbi? =
+    fun jdbiBean(dataSource: DataSource): Jdbi =
             Jdbi.create(dataSource)
                     // Plugins
                     .installPlugin(KotlinPlugin())
                     .installPlugin(KotlinSqlObjectPlugin())
+
+    @Bean(destroyMethod = "close")
+    @RequestScope
+    fun jdbiHandleBean(jdbi: Jdbi): Handle {
+        val handle = jdbi.open()
+//        handle.begin()
+        return handle
+    }
 }
