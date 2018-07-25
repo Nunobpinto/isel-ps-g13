40456d092d6fbf298107cbb2195a9753fd314ca2
brunofilipe <bruno.filipe1996@hotmail.com>
2018-07-25 21:50:35 +0100

    added votes for exams and changed Home (web app)

--- front-end/eduwiki/src/components/Exams.js
+++ front-end/eduwiki/src/components/Exams.js
@@ -1,15 +1,107 @@
 import React from 'react'
+import fetch from 'isomorphic-fetch'
 import {Card, Col, Row, Upload, Button, Icon} from 'antd'
 
 export default class extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      up: false,
+      down: false,
+      exams: props.exams,
+      courseId: props.courseId,
+      termId: props.termId
+    }
+    this.voteUp = this.voteUp.bind(this)
+    this.voteDown = this.voteDown.bind(this)
+  }
+
+  voteUp () {
+    const voteInput = {
+      vote: 'Up',
+      created_by: 'ze'
+    }
+    const examId = this.state.id
+    const url = `http://localhost:8080/courses/${this.state.courseId}/terms/${this.state.termId}/exams/${this.state.id}/vote`
+    const body = {
+      method: 'POST',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(voteInput)
+    }
+    fetch(url, body)
+      .then(resp => {
+        if (resp.status >= 400) {
+          throw new Error('Unable to access content')
+        }
+        this.setState(prevState => {
+          let newArray = [...prevState.exams]
+          const index = newArray.findIndex(exam => exam.examId === examId)
+          newArray[index].votes = prevState.exams[index].votes + 1
+          return ({
+            exams: newArray,
+            up: false
+          })
+        })
+      })
+  }
+
+  voteDown () {
+    const voteInput = {
+      vote: 'Down',
+      created_by: 'ze'
+    }
+    const examId = this.state.id
+    const url = `http://localhost:8080/courses/${this.state.courseId}/terms/${this.state.termId}/exams/${this.state.id}/vote`
+    const body = {
+      method: 'POST',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(voteInput)
+    }
+    fetch(url, body)
+      .then(resp => {
+        if (resp.status >= 400) {
+          throw new Error('Unable to access content')
+        }
+        this.setState(prevState => {
+          let newArray = [...prevState.exams]
+          const index = newArray.findIndex(exam => exam.examId === examId)
+          newArray[index].votes = prevState.exams[index].votes - 1
+          return ({
+            exams: newArray,
+            down: false
+          })
+        })
+      })
+  }
+
+  componentWillReceiveProps (props) {
+    this.setState({
+      exams: props.exams,
+      courseId: props.courseId,
+      termId: props.termId
+    })
+  }
+
   render () {
     return (
       <div id={`exms_term_${this.props.termId}`}>
-        {this.props.exams.map(exam => (
+        {this.state.exams.map(exam => (
           <div style={{ padding: '30px' }}>
             <Row gutter={16}>
-              <Col span={8} key={exam.id}>
-                <Card title={`${exam.type} - ${exam.phase} - ${exam.dueDate}`}>
+              <Col span={8} key={exam.examId}>
+                <Card
+                  title={`${exam.type} - ${exam.phase} - ${exam.dueDate} - ${exam.votes}`}
+                  actions={[
+                    <Button id='like_btn' shape='circle' icon='like' onClick={() => this.setState({up: true, id: exam.examId})} />,
+                    <Button id='dislike_btn' shape='circle' icon='dislike' onClick={() => this.setState({down: true, id: exam.examId})} />
+                  ]}
+                >
                   {exam.sheet}
                 </Card>
               </Col>
@@ -23,5 +115,13 @@ export default class extends React.Component {
         </Upload>
       </div>
     )
+  }
+
+  componentDidUpdate () {
+    if (this.state.up) {
+      this.voteUp()
+    } else if (this.state.down) {
+      this.voteDown()
+    }
   }
 }
--- front-end/eduwiki/src/components/Home.js
+++ front-end/eduwiki/src/components/Home.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Card, Col, Row } from 'antd'
 import Login from './Login'
 import { Link } from 'react-router-dom'
-const { Meta } = Card
 
 export default (props) => {
   return (
@@ -10,52 +8,6 @@ export default (props) => {
       <img alt='EduWiki Logo' id='home-logo' src='logo_color.png' />
       <div>
         <Login />
-        <Row>
-          <Col span={5}>
-            <Card
-              style={{ width: 240 }}
-              cover={<img alt='Academic Resources' src='http://www.lourdes.edu/wp-content/uploads/2015/12/EnglishStudies-913x513.jpg' />}
-            >
-              <Meta
-                title='Manage Academic Resources'
-                description='Here you can check classes, exams, lectures, academic schedule and more'
-              />
-            </Card>
-          </Col>
-          <Col span={5}>
-            <Card
-              style={{ width: 240 }}
-              cover={<img alt='Community Driven' src='http://stagtv.co.uk/wp-content/uploads/2017/07/group-of-users-silhouette_318-49953.jpg' />}
-            >
-              <Meta
-                title='Community Driven'
-                description='Manage whole data in community, the more you interact, higher your reputation'
-              />
-            </Card>
-          </Col>
-          <Col span={5}>
-            <Card
-              style={{ width: 240 }}
-              cover={<img alt='Report' src='https://cdn0.iconfinder.com/data/icons/education-vol-02-2/32/report-student-grade-wrong-paper-exam-result-512.png' />}
-            >
-              <Meta
-                title='Report Information'
-                description='If something isn´t right you may report so that admin users will evaluate it'
-              />
-            </Card>
-          </Col>
-          <Col span={5}>
-            <Card
-              style={{ width: 240 }}
-              cover={<img alt='Academic Email' src='https://fthmb.tqn.com/X7zpxW-USzMwCySRa3JZhbO3vVM=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/768px-Email_Shiny_Icon.svg-57fd8b3a3df78c690f82ca98.png' />}
-            >
-              <Meta
-                title='Use academic Email'
-                description='Use your own academic email to authenticate yourself as a University Student'
-              />
-            </Card>
-          </Col>
-        </Row>
       </div>
       <h1>Current web pages</h1>
       <ul>
