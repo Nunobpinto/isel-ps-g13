1476b2a4b53a8cf3f01c1471f85c93b04a2ccae7
brunofilipe <bruno.filipe1996@hotmail.com>
2018-08-03 22:18:18 +0100

    adding xml to android app

---
+++ front-end/eduwikiMobile/app/src/main/java/isel/leic/ps/g13/eduwikimobile/User.kt
@@ -0,0 +1,6 @@
+package isel.leic.ps.g13.eduwikimobile
+
+data class User (
+        val username: String,
+        val password: String
+)
---
+++ front-end/eduwikiMobile/app/src/main/java/isel/leic/ps/g13/eduwikimobile/ui/ProgrammeActivity.kt
@@ -0,0 +1,13 @@
+package isel.leic.ps.g13.eduwikimobile.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import isel.leic.ps.g13.eduwikimobile.R
+
+class ProgrammeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_programme)
+    }
+}
